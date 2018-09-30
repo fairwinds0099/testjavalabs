a82270b8ad84131b0f9eb97e8e07eb0760b7103c
gx <fairwinds0099@gmail.com>
2018-09-30 12:45:33 -0500

    TODO added for database connections

--- phaseone/src/main/java/HelloWorld.java
+++ phaseone/src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
+import org.junit.jupiter.api.Test;
 
-public class HelloWorld {
+public class HelloWorld extends Utils{
 
   @Test
   public void myFirstTest() {
--- phaseone/src/main/java/Utils.java
+++ phaseone/src/main/java/Utils.java
@@ -1,7 +1,7 @@
 
 public class Utils {
 
-  private void forLoopTenTimes() {
+  public void forLoopTenTimes() {
     for (int i = 0; i < 10; i++) {
       System.out.println("Hello World");
     }
@@ -14,5 +14,12 @@ public class Utils {
   public static void connectToDatabase() {
     System.out.println("This method connectes to a remote database");
   }
+
+  // TODO please add a method to connecte to the below database
+  //String hostName = "pos-experiment.cn1e1hoy5jio.us-east-1.rds.amazonaws.com";
+  //    int port = 1521;
+  //    String dbName = "POS";
+  //    String userName = "pos_user";
+  //    String password = "Sb123456";
 
 }
