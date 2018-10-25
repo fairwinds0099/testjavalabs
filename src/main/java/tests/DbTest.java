63ae6c191ef2a3be476449f142e1e2fb2cb32bc8
aliisiklar2001@icloud.com <aliisiklar2001@icloud.com>
2018-10-25 07:16:06 -0700

    tasks completed

--- src/main/java/tests/DbTest.java
+++ src/main/java/tests/DbTest.java
@@ -2,12 +2,25 @@ package tests;
 
 
 import org.testng.annotations.Test;
+import utils.DbUtils.Dao;
+
+import java.sql.SQLException;
 
 // TODO updated
-public class DbTest {
+public class DbTest{
+
 
     @Test
-    public void myTest() {
+    public void myTest() throws SQLException {
+
+        Dao dbCon = new Dao();
+        dbCon.createQuery();
+        dbCon.deleteQuery();
+        dbCon.updateQuery();
+        dbCon.readQuery();
+
+
+
 
     }
 }
--- src/main/java/utils/DbUtils/Dao.java
+++ src/main/java/utils/DbUtils/Dao.java
@@ -1,12 +1,6 @@
 package utils.DbUtils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-import java.lang.Class;
+import java.sql.*;
 
 public class Dao {
         //TODO: create a new branch
@@ -24,18 +18,67 @@ public class Dao {
         final String queryDelete = "DELETE FROM ADDRESS WHERE ID=7";
 
         // let us extract this and make a method
+        //Class.forName("oracle.jdbc.driver.OracleDriver");(Bunu kullanmama gerek var mi bilemedim, hata verip duruyor.
         Connection conn = DriverManager.getConnection(dbUri, userName, password);
-        Statement statement = conn.prepareStatement(queryCreate);
+        Statement statement = conn.createStatement();
 
-        statement.executeQuery(queryCreate);
-        statement.executeQuery(queryUpdate);
-        statement.executeQuery(queryDelete);
+    public Dao() throws SQLException {
+    }
+
+
+    //ResultSet rs1 = statement.executeQuery(queryCreate);
+        //ResultSet rs2 = statement.executeQuery(queryUpdate);
+        //ResultSet rs3 = statement.executeQuery(queryDelete);
+
+                public void updateQuery () throws SQLException {
+                    try {
+                        statement.executeQuery(queryUpdate);
+                    } catch (SQLException e) {
+                        e.getErrorCode();
+                    }
+                }
+                public void createQuery () throws SQLException {
+                    try {
+                        statement.executeQuery(queryCreate);
+                    } catch (SQLException e) {
+                        e.getStackTrace();
+                    }
+                }
+                public void deleteQuery () throws SQLException {
+                    try {
+                        statement.executeQuery(queryDelete);
+                    } catch (SQLDataException e) {
+                        e.getStackTrace();
+                    }
+                }
+
+            //if (query == queryCreate){
+              //  ResultSet rs1 = statement.executeQuery(queryCreate);
+                //return rs1;
+            //}
+            //if (query == queryUpdate){
+              //  ResultSet rs2 = statement.executeQuery(queryUpdate);
+                //return rs2;
+            //}
+            //if (query == queryDelete){
+              //  ResultSet rs3 = statement.executeQuery(queryDelete);
+                //return rs3;
+            //}
+
+
+            //return null;
+
 
         // do not leave blanks between lines
         // as you will see these are all executequery methods
         // you can extract  an execute sql query methods
-        Statement statement = conn.prepareStatement(queryRead);
-        ResultSet rs = statement.executeQuery(queryRead);
+
+
+        public ResultSet readQuery() throws SQLException {
+            Statement statement = conn.prepareStatement(queryRead);
+            ResultSet rs = statement.executeQuery(queryRead);
+            return rs;
+        }
         // except this one read. Because when you read you get something back and  need to get that info
         // call this method in DbTest
         // and do some exception handling
