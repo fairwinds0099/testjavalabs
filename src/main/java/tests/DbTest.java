af596dcf28783292a3408cfefa4fab039257eb62
aliisiklar2001@icloud.com <aliisiklar2001@icloud.com>
2018-10-25 20:29:22 -0700

    tasks completed

--- src/main/java/tests/DbTest.java
+++ src/main/java/tests/DbTest.java
@@ -9,15 +9,20 @@ import java.sql.SQLException;
 // TODO updated
 public class DbTest{
 
+    final String queryCreate = "INSERT INTO ADDRESS (ID, STREETNAME, CITY, ZIPCODE, STATE)" +
+            "VALUES(7, '311 True St', 'Richmond', '54544', 'VA')";
+    final String queryRead = "SELECT * FROM ADDRESS WHERE ZIPCODE=54544";
+    final String queryUpdate = "UPDATE ADDRESS SET CITY = 'Miami', ZIPCODE='22222' WHERE ID='7'";
+    final String queryDelete = "DELETE FROM ADDRESS WHERE ID=7";
 
     @Test
     public void myTest() throws SQLException {
 
-        Dao dbCon = new Dao();
-        dbCon.createQuery();
-        dbCon.deleteQuery();
-        dbCon.updateQuery();
-        dbCon.readQuery();
+        Dao dao = new Dao();
+        dao.createQuery(queryCreate);
+        dao.deleteQuery(queryDelete);
+        dao.updateQuery(queryUpdate);
+        dao.readQuery(queryRead);
 
 
 
--- src/main/java/utils/DbUtils/Dao.java
+++ src/main/java/utils/DbUtils/Dao.java
@@ -3,88 +3,50 @@ package utils.DbUtils;
 import java.sql.*;
 
 public class Dao {
-        //TODO: create a new branch
-
 
         String dbUri = "jdbc:oracle:thin:@//florida.csr9s71rdbw2.us-east-1.rds.amazonaws.com:1521/FLORIDA";
         String userName = "florida";
         String password = "miamiflorida";
 
-        // We need to make these variables final so they cannot be changed by someone else
-        final String queryCreate = "INSERT INTO ADDRESS (ID, STREETNAME, CITY, ZIPCODE, STATE)" +
-                "VALUES(7, '311 True St', 'Richmond', '54544', 'VA')";
-        final String queryRead = "SELECT * FROM ADDRESS WHERE ZIPCODE=54544";
-        final String queryUpdate = "UPDATE ADDRESS SET CITY = 'Miami', ZIPCODE='22222' WHERE ID='7'";
-        final String queryDelete = "DELETE FROM ADDRESS WHERE ID=7";
-
-        // let us extract this and make a method
-        //Class.forName("oracle.jdbc.driver.OracleDriver");(Bunu kullanmama gerek var mi bilemedim, hata verip duruyor.
         Connection conn = DriverManager.getConnection(dbUri, userName, password);
         Statement statement = conn.createStatement();
 
     public Dao() throws SQLException {
     }
-
-
-    //ResultSet rs1 = statement.executeQuery(queryCreate);
-        //ResultSet rs2 = statement.executeQuery(queryUpdate);
-        //ResultSet rs3 = statement.executeQuery(queryDelete);
-
-                public void updateQuery () throws SQLException {
+                public void updateQuery (String queryupdate){
                     try {
-                        statement.executeQuery(queryUpdate);
+                        Connection conn = DriverManager.getConnection(dbUri, userName, password);
+                        Statement statement = conn.createStatement();
+                                statement.executeQuery(queryupdate);
                     } catch (SQLException e) {
                         e.getErrorCode();
                     }
                 }
-                public void createQuery () throws SQLException {
+                public void createQuery(String querycreate){
                     try {
-                        statement.executeQuery(queryCreate);
+                        Connection conn = DriverManager.getConnection(dbUri, userName, password);
+                        Statement statement = conn.createStatement();
+                                statement.executeQuery(querycreate);
                     } catch (SQLException e) {
-                        e.getStackTrace();
+                        e.getErrorCode();
                     }
                 }
-                public void deleteQuery () throws SQLException {
+                public void deleteQuery (String querydelete) {
                     try {
-                        statement.executeQuery(queryDelete);
-                    } catch (SQLDataException e) {
-                        e.getStackTrace();
+                        Connection conn = DriverManager.getConnection(dbUri, userName, password);
+                        Statement statement = conn.createStatement();
+                                statement.executeQuery(querydelete);
+                    } catch (SQLException e) {
+                        e.getErrorCode();
                     }
                 }
-
-            //if (query == queryCreate){
-              //  ResultSet rs1 = statement.executeQuery(queryCreate);
-                //return rs1;
-            //}
-            //if (query == queryUpdate){
-              //  ResultSet rs2 = statement.executeQuery(queryUpdate);
-                //return rs2;
-            //}
-            //if (query == queryDelete){
-              //  ResultSet rs3 = statement.executeQuery(queryDelete);
-                //return rs3;
-            //}
-
-
-            //return null;
-
-
-        // do not leave blanks between lines
-        // as you will see these are all executequery methods
-        // you can extract  an execute sql query methods
-
-
-        public ResultSet readQuery() throws SQLException {
-            Statement statement = conn.prepareStatement(queryRead);
-            ResultSet rs = statement.executeQuery(queryRead);
+                
+        public ResultSet readQuery(String queryread) throws SQLException {
+            Statement statement = conn.prepareStatement(queryread);
+            ResultSet rs = statement.executeQuery(queryread);
             return rs;
         }
-        // except this one read. Because when you read you get something back and  need to get that info
-        // call this method in DbTest
-        // and do some exception handling
 
-
-        //TODO post this part is added after Ali's merge
 }
 
 
