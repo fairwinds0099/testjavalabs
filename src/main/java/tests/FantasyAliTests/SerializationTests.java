f9ddd2c85f2d1fe9deaf61fc7f38729f3a901187
gx <fairwinds0099@gmail.com>
2018-12-08 23:31:32 -0600

    ser and deser

--- src/main/java/tests/FantasyAliTests/SerializationTests.java
+++ src/main/java/tests/FantasyAliTests/SerializationTests.java
@@ -116,26 +116,5 @@ public class SerializationTests {
         return gson.fromJson(json, pojo);
     }
 
-    private String getFileContent(String filePath) {
-        StringBuilder result = new StringBuilder();
-
-        //retrieve the  file from path
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(filePath).getFile());
-
-        Scanner scanner = null;
-
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            //do nothing
-        }
-        while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            result.append(line);
-            scanner.close();
-        }
-        return result.toString();
-    }
-
+   
 }
