4d8272f4dbfe115c7fe06b6e4e49000ddbc647a8
gx <fairwinds0099@gmail.com>
2019-06-11 17:05:51 -0500

    gitignore added Synch utils added

---
+++ src/test/java/utils/SynchUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.awaitility.Awaitility;
+import org.awaitility.Duration;
+
+public class SynchUtils {
+
+    /*
+    used to synchronize processes
+     */
+    public static void waitUntilProcessComplete(boolean condition) {
+        Awaitility
+                .with()
+                .pollInterval(Duration.FIVE_SECONDS)
+                .await()
+                .atMost(Duration.FIVE_MINUTES)
+                .until(() -> condition);
+    }
+}
